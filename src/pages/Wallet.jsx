8ae7284409d2b21678b0be109a4f1df2ba3cc6bc
Vinicius José <viniciusjosedev@gmail.com>
2023-02-01 21:07:47 -0300

    fix: fixed lint errors

--- src/pages/Wallet.jsx
+++ src/pages/Wallet.jsx
@@ -1,18 +1,17 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Wallet extends React.Component {
   render() {
-		const { email } = this.props;
+    const { email } = this.props;
     return (
-			<>
       <header>
-				<h1>TrybeWallet</h1>
-				<p data-testid="email-field">{email}</p>
-				<p data-testid="total-field">0</p>
-				<p data-testid="header-currency-field">BRL</p>
-				</header>
-			</>
+        <h1>TrybeWallet</h1>
+        <p data-testid="email-field">{email}</p>
+        <p data-testid="total-field">0</p>
+        <p data-testid="header-currency-field">BRL</p>
+      </header>
     );
   }
 }
@@ -22,3 +21,7 @@ const mapStateToProps = (state) => ({
 });
 
 export default connect(mapStateToProps)(Wallet);
+
+Wallet.propTypes = {
+  email: PropTypes.string.isRequired,
+};
