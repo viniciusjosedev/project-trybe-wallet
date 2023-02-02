0c0f47626127833faaceb80ce4af33c2dd16edbb
Vinicius José <viniciusjosedev@gmail.com>
2023-02-01 21:03:20 -0300

    requirement 2 done

--- src/pages/Wallet.jsx
+++ src/pages/Wallet.jsx
@@ -3,8 +3,22 @@ import { connect } from 'react-redux';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+		const { email } = this.props;
+    return (
+			<>
+      <header>
+				<h1>TrybeWallet</h1>
+				<p data-testid="email-field">{email}</p>
+				<p data-testid="total-field">0</p>
+				<p data-testid="header-currency-field">BRL</p>
+				</header>
+			</>
+    );
   }
 }
 
-export default connect()(Wallet);
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+export default connect(mapStateToProps)(Wallet);
