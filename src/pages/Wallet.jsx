3334b879438c274dca37bdf95a219cb8de585a71
Vinicius José <viniciusjosedev@gmail.com>
2023-02-03 12:43:28 -0300

    requirement 6 done

--- src/pages/Wallet.jsx
+++ src/pages/Wallet.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 // import PropTypes from 'prop-types';
 import Header from '../components/Header';
+import Table from '../components/Table';
 import WalletForm from '../components/WalletForm';
 
 class Wallet extends React.Component {
@@ -10,6 +11,7 @@ class Wallet extends React.Component {
       <>
         <Header />
         <WalletForm />
+        <Table />
       </>
     );
   }
